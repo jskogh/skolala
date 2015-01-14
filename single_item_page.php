268371e4de547b6c06f22252f231efe245c24569
Johanna Skogh <johanna.skogh@gmail.com>
2015-01-14 13:01:37 +0100

    new images

--- single_item_page.php
+++ single_item_page.php
@@ -29,5 +29,34 @@
 					</div>
 					
 				</div>
-				
+								
 			</div>
+			
+			
+			<div id="single_item_page">
+				
+				<div id="product_image">
+					<img src="img/shoes/img_01.png" alt="product" />
+					
+					<div id="more_images">
+					
+					</div>
+					
+					
+				</div>
+				
+				<div id="product_info">
+					<h2>Namn på sko</h2>
+				</div>
+				
+				<div id="releted_products">
+				
+				</div>
+				
+				<div id="environmental_stuff">
+				
+				</div>
+				
+			</div>	
+
+		</div>
