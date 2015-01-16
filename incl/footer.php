d5f6f6bd49ec45d4e82a16b41b77f4f0dc2e33ca
Johanna Skogh <johanna.skogh@gmail.com>
2015-01-16 09:29:42 +0100

    layout fix in singlepage

---
+++ incl/footer.php
@@ -0,0 +1,26 @@
+			<div id="footer">
+				<div id="important_links">
+				
+				</div>
+				
+				<div id="contact_info">
+				
+				</div>
+				
+				<div id="payments">
+				
+				</div>
+				
+				<div id="copyrights">
+				
+				</div>
+				
+				
+			</div>
+		
+		</div> <!-- end wrapper -->
+		
+	</body> 
+
+</html> 
+
--- single_item_page.php
+++ single_item_page.php
@@ -1,22 +1,81 @@
+<?php
+	use app\models\User;
+	use app\models\Shoes;
+	use app\DB;
+	
 
+	require_once 'app/start.php';
+
+
+
+$shoes = new Shoes();
+	
+
+?>
 
 				<?php include("incl/header.php"); ?>
 
 				<div id="single_item_page">
 				
-				<div id="product_image">
-					<img src="img/shoes/img_01.png" alt="product" />
-					
-					<div id="more_images">
-					
-					</div>
-					
-					
-				</div>
+					<div id="product">
 				
-				<div id="product_info">
-					<h2>Namn på sko</h2>
-				</div>
+						<div id="product_image">
+							<img src="img/shoes/img_01.png" alt="product" />
+							
+							<div id="more_images">
+								<img src="img/shoes/img_01_02.png" alt ="shoe1" /> 
+								<img src="img/shoes/img_01_03.png" alt ="shoe2" /> 
+								<img src="img/shoes/img_01.png" alt ="shoe3" /> 
+							</div>
+							
+							
+						</div>
+						
+						<div id="product_info">
+							<p class="product_name"> SkoEtt </p>
+							
+							<p class="brand"> Märke: Kavat </p>
+							
+							<p class="materials"> Material: Läder, Skinn </p>
+							
+							<p class="about_product"> Massa om skon lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed blandit sem. Nam lacinia quis sem tempus cursus. Duis sed ipsum nec sem laoreet vulputate. Maecenas porta, urna ac dapibus porttitor, massa quam placerat nisi, et mollis ligula mi ac purus. Phasellus risus est, dictum ac est sed, rutrum imperdiet ipsum. Curabitur ac lorem in sem egestas laoreet. Vestibulum tincidunt pharetra lacinia </p>
+							
+							<p class="environment_info"> Påverkan på miljön: 1478 blabla utsläppt </p>
+							
+							<p class="price"> 1299 SEK </p>
+							
+							<p class="options">
+								<form>
+									<select name="shoe_size">
+										<option>Storlek </option>
+										<option>40 </option>
+										<option>41 </option>
+										<option>42 </option>
+										<option>43 </option>
+									</select>
+									<select name="number_of_shoes">
+										<option>Antal </option>
+										<option>1 </option>
+										<option>2 </option>
+										<option>3 </option>
+										<option>4 </option>
+									</select> <br />
+									
+									<input type="submit" value="Lägg till i varukorg" name="add_to_cart" />
+								</form>
+							</p>
+							
+							<div id="social_media_logos_single_item">
+								<img src="img/logos1/twitter1.png" alt ="twitter" /> 
+								<img src="img/logos1/facebook1.png" alt ="twitter" /> 
+								<img src="img/logos1/instagram1.png" alt ="twitter" /> 
+								<img src="img/logos1/pinterest1.png" alt ="twitter" /> 
+							</div>
+							
+							<p> </p>
+						</div>
+						
+					</div>
 				
 				<div id="releted_products">
 				
@@ -27,7 +86,5 @@
 				</div>
 				
 			</div>	
-
-		</div> <!-- end wrapper -->
-		
-	</body>
+			
+		<?php include("incl/footer.php") ?>
