7b1c5e8d366e1222586006f6a6970c4391e3b6cb
Johanna Skogh <johanna.skogh@gmail.com>
2015-01-30 14:23:24 +0100

    footer fix

--- incl/footer.php
+++ incl/footer.php
@@ -1,31 +1,40 @@
 			<div id="footer">
 				<div id="important_links">
 					<ul>
-						<li>Om oss  | </li>
-						<li>Plantera träd  | </li>
-						<li></li>
-						<li></li>
-						<li></li>
-						
+						<li><a href="about.php"> Om oss </a>  </li>
+						<li> | </li>
+						<li> <a href="philosophy.php">Plantera träd </a> </li>
+						<li> | </li>
+						<li> <a href="blogg.php"> Miljöbloggen </a> </li>
+						<li> | </li>
+						<li> <a href="my_page.php">Min sida </a> </li>
+						<li> | </li> 
+						<li> <a href="#"> Köpvillkor </a></li>
+						<li> | </li>
+						<li> <a href="#"> Vanliga Frågor</a></li>
 					</ul>
 				</div>
 				
 				<div id="contact_info">
 				
 					<ul>
-						<li>info@ecoshoes.se | </li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						
+						<li> <a href="mailto:info@ecoshoes.se"> info@ecoshoes.se </a> </li>				
 					</ul>
 					
 				</div>
 				
 				<div id="payments">
 					<ul class="display_inline">
-						<li> </li>
+					<img src="http://www.junkyard.se/media/wysiwyg//icons_misc/trygg-e-handel-SE.png" alt="">
+					<img src="http://www.junkyard.se/media/wysiwyg/icons_misc/betalalternativ_SE/payment-etc_SE_01.png" alt="">
+					<img src="http://www.junkyard.se/media/wysiwyg/icons_misc/betalalternativ_SE/payment-etc_SE_02.png" alt="">
+					<img src="http://www.junkyard.se/media/wysiwyg/icons_misc/betalalternativ_SE/payment-etc_SE_03.png" alt="">
+					<img src="http://www.junkyard.se/media/wysiwyg/icons_misc/betalalternativ_SE/payment-etc_SE_04.png" alt="">
+					<img src="http://www.junkyard.se/media/wysiwyg/icons_misc/betalalternativ_SE/payment-etc_SE_05.png" alt="">
+					<img src="http://www.junkyard.se/media/wysiwyg/icons_misc/betalalternativ_SE/payment-etc_SE_06.png" alt="">
+					<img src="http://www.junkyard.se/media/wysiwyg/icons_misc/betalalternativ_SE/payment-etc_SE_07.png" alt="">
+					<img src="http://www.junkyard.se/media/wysiwyg/icons_misc/betalalternativ_SE/payment-etc_SE_08.png" alt="">
+					<img src="http://www.junkyard.se/media/wysiwyg//icons_misc/trygg-e-handel-SE.png" alt="">
 					</ul>
 				</div>
 				
